ef1be7cac85dae288dd5f42556c4dd334031b53e
hrs00 <135930294+hrs00@users.noreply.github.com>
2023-06-10 21:54:04 +0530

    Update client.py

--- client.py
+++ client.py
@@ -19,7 +19,7 @@ except:
 def write():
     while True:
         try:
-            data = input()
+            data = "check" + input()
             conn.sendall(data.encode())
         except:
             return
